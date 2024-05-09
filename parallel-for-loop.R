6223ee36e0b056c7527c01a0a75f2786955a5cde
Garrett Otrimski <gotrimski@gmail.com>
2024-05-09 15:54:29 -0600

    more plot edits plus parallelization of for

---
+++ parallel-for-loop.R
@@ -0,0 +1,31 @@
+#install.packages("foreach")
+
+
+if (!exists("n.cores")) {
+
+"initilizing cores..."
+  n.cores <- parallel::detectCores() - 1
+  my.cluster <- parallel::makeCluster(
+    n.cores, 
+    type = "PSOCK"
+  )
+  doParallel::registerDoParallel(cl = my.cluster)
+  
+  #check if it is registered (optional)
+  foreach::getDoParRegistered()
+  
+"parallel cores initialized."
+
+}
+
+your_vector<-c(1:10)
+
+
+foreach(i=1:length(your_vector)) %dopar% {
+  
+#if you use a library you have to load it within the loop
+#(or specify command with ::)
+
+print(i)
+  
+}
