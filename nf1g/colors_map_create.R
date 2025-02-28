04e491e96dcd624f643039514a683d2078a44001
Garrett Otrimski <u1413890@hci.utah.edu>
2025-02-28 09:22:40 -0700

    colors map 1

--- nf1g/colors_map_create.R
+++ nf1g/colors_map_create.R
@@ -9,12 +9,8 @@ library(survival)
 library(survminer)
 library(RColorBrewer)
 
-
-
-
-
 num_categories<-6
-curve_category_names<-readRDS("nf1g/surv/cohorts-2025-01-07.rds")%>%select(resultant_geno)%>%arrange(resultant_geno)%>%unique()%>%pull()
+curve_category_names<-readRDS("nf1g/surv/cohorts-2025-02-27.rds")%>%select(resultant_geno)%>%arrange(resultant_geno)%>%unique()%>%pull()
 
 
 geno_colors_map <- list(resultant_geno=setNames(hue_pal()(num_categories), curve_category_names))
@@ -31,6 +27,33 @@ geno_colors_map<-list(resultant_geno=setNames(c("#B79F00",
 saveRDS(geno_colors_map, "nf1g/ds/colors_map_geno.rds")
 
 
+
+
+
+
+
+
+library(googlesheets4)
+sheet_id<-"https://docs.google.com/spreadsheets/d/1SALlvQdI5zyhZLgUg3XS1kmb2M8Skr_8Pjt9y_spWGE/edit?gid=667454129#gid=667454129"
+gs4_auth(email = "gotrimski@gmail.com") 
+
+
+for (sn in 1:length(sheet_names(sheet_id))){
+
+
+name<-sheet_names(sheet_id)[sn]  
+  
+assign(x=paste0("s_",name),
+  
+  value=read_sheet(sheet_id, sheet = name) %>%
+  mutate(across(everything(), as.character)) %>%
+  mutate(across(everything(), ~ na_if(.x, "NULL"))) %>%
+  janitor::clean_names() %>%
+  select(2,5) %>%
+  filter(!is.na(.[[1]]))
+  
+)
+}
 
 
 
