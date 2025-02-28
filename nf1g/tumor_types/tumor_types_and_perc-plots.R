901c052ebaa21d0349f71d2617df322333db3ba9
Garrett Otrimski <u1413890@hci.utah.edu>
2025-02-28 09:22:40 -0700

    colors map 1

--- nf1g/tumor_types/tumor_types_and_perc-plots.R
+++ nf1g/tumor_types/tumor_types_and_perc-plots.R
@@ -12,151 +12,40 @@ library(RColorBrewer)
 
 ########dataset read in and construction######################
 #reading in current dataset. 
-coh1<-readRDS("nf1g/surv/cohorts-2025-02-27.rds")%>%
+coh1<-readRDS("nf1g/surv/cohorts-2025-02-27.rds")
 
-  
-  
-  
-    mutate(aod = as.numeric(aod))%>%
-
-
+df1<-coh1%>%
 #most conservative exclusion criteria
 filter(is.na(exclude)|exclude>3)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-df0<-coh1%>%
-
-  mutate(event = if_else(is.na(exp_end_date),1,0))%>%
-  select(-exp_end_date)%>%
-  
-  #making a convenient variable to easily compare nf1 vs. non-nf1s. 
-  mutate(nf1=substr(resultant_geno, 1, 6))%>%
-  mutate(geno_bg=substr(resultant_geno, 8, nchar(resultant_geno)))%>%
-  
-  #most conservative exclusion criteria
-  filter(is.na(exclude)|exclude>3)%>%
-  
-  mutate(aod=if_else(event==0, 150,aod))%>%
-  
-  mutate(hist_cat=substr(hist, 1,1))%>%
-  mutate(hist_grade=substr(hist, 3,4))%>%
-  mutate(hist_grade=gsub("\\.", "", hist_grade))%>%
-  mutate(hist_grade=gsub("15", "1.5", hist_grade))%>%
-  mutate(hist_grade=gsub("^d$", "ned", hist_grade))%>%
-  
-  
-  
-  mutate(hist_catgrade=paste0(hist_cat, ".", hist_grade))%>%
-  mutate(hist_catgrade=if_else(hist_catgrade=="n.ned", "ned", hist_catgrade))%>%
-  
-  mutate(hist_cat=if_else(hist_cat=="n", "ned", hist_cat))%>%
-  mutate(hist_grade=if_else(hist_grade=="N", "no grade assigned", hist_grade))
-
-
-
-nf1_renaming_dataset1 <- readxl::read_excel("nf1g/surv/nf1-renaming dataset.xlsx")
-nf1_renaming_dataset2 <- readxl::read_excel("nf1g/surv/nf1-renaming dataset.xlsx", sheet = "resultant_geno")
-nf1_renaming_dataset3 <- readxl::read_excel("nf1g/surv/nf1-renaming dataset.xlsx", sheet = "patho_cat")
-#nf1_renaming_dataset4 <- readxl::read_excel("nf1g/surv/nf1-renaming dataset.xlsx", sheet = "patho_cat2")
-nf1_renaming_dataset5 <- readxl::read_excel("nf1g/surv/nf1-renaming dataset.xlsx", sheet = "patho_cat_det")%>%
-  rename(hist=patho_cat_det)
-
-df1<-df0%>%
-  left_join(nf1_renaming_dataset1)%>%
-  left_join(nf1_renaming_dataset2)%>%
-  left_join(nf1_renaming_dataset3)%>%
-  #left_join(nf1_renaming_dataset4)
-  left_join(nf1_renaming_dataset5)%>%
-  mutate(full_cohort="1")%>%
-  mutate(patho_cat_name=if_else(hist=="3.1", "Pretumorigenic", patho_cat_name))%>%
-
-
-
-mutate(hist=if_else(!is.na(exclude_from_hist_reason)&event==1, "excluded from hist (event)", hist))%>%
-  mutate(hist=if_else(!is.na(exclude_from_hist_reason)&event==0, "excluded from hist (no event)", hist))%>%
-
-
-
-name<-df1%>%select(patho_cat_name)%>%unique()
-
-
-name_order<-c("Gliomas", "Nerve sheath tumors","Spindle and epithelioid tumors",
-"Glioneuronal tumors",    "Pretumorigenic",  "No histological classification", "No evidence of disease")       
-
-
-
-
-df1<-df1%>%
-  mutate(patho_cat_name=factor(patho_cat_name, levels=name_order))
-
 
 
 aspectratio<-.6
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 genotypes<-df1%>%select(resultant_geno)%>%unique()%>%arrange()%>%pull()
 
 
-geno_subset<-df1%>%
- #filter(resultant_geno==genotypes[1]|resultant_geno==genotypes[2])%>%
-  select(resultant_geno, patho_cat_name)%>%
-  group_by(resultant_geno,patho_cat_name)%>%
-  summarise(n=n(),
-            )%>%
-  ungroup()%>%
+
+
+
+df_props<-df1%>%
+
+  select(resultant_geno, hist_cat_name)%>%
+  group_by(resultant_geno,hist_cat_name)%>%
+  
+  #count occurrences of each hist_cat per geno.
+  reframe(n=n() )%>%
+  
+  #calculate percentage/likelihood of hist_cat per geno.
   group_by(resultant_geno)%>%
-  summarize(patho_cat_name, n=n,total_n=sum(n))%>%
+  reframe(hist_cat_name, n=n,total_n=sum(n))%>%
   mutate(perc=n/total_n*100)%>%
-  ungroup()%>%
-  complete(resultant_geno, patho_cat_name, fill = list(perc = -1))
 
-
-
-geno_subset
-
-
-prop_test_results <- geno_subset %>%
-  group_by(patho_cat_name) %>%
-  summarise(
-    p_value = prop.test(
-      x = n,  # The count of successes (e.g., number of "green" individuals)
-      n = total_n  # The total sample size
-    )$p.value  # Extract p.value directly from the result
-  )
-
-
-
+  #add in a dummy number for each hist_cat-geno combo to ensure consistency in plot display groups.
+  complete(resultant_geno, hist_cat_name, fill = list(perc = -.5))%>%
+  
+  #get back info for resultant_geno_name ("proper" cohort names), to use if desired.
+  left_join(coh1%>%select(resultant_geno, resultant_geno_name)%>%unique())
 
 
 
@@ -382,5 +271,21 @@ ggsave("nf1g/tumor_types/tumor_types-2.pdf",
   # theme(plot.title = element_text(hjust = 0),
   #       aspect.ratio=aspectratio,
   #       plot.margin = margin(5, 5, 40, 5))+
+
+
+
+
+# prop_test_results <- df_props %>%
+#   group_by(hist_cat_name, resultant_geno) %>%
+#   reframe(
+#     p_value = tryCatch({
+#       test <- prop.test(x = n, n = total_n)
+#       test$p.value
+#     }, error = function(e) NA)  # Return NA if there's an error
+#   )
+
+
+
+
 
 
