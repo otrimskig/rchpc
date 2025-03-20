02718ee3ffe081e8a2642bca8483b0f784628e9c
Garrett Otrimski <gotrimski@gmail.com>
2025-03-20 17:34:04 -0600

    gostplot edits with labels

--- nf1g/gost/2025-03-17-custom_gprof-atrx-noninteractive-loops.R
+++ nf1g/gost/2025-03-17-custom_gprof-atrx-noninteractive-loops.R
@@ -9,6 +9,7 @@ library(ggpp)
 library(crayon)
 library(grid)
 library(gridExtra)
+library(colorspace)
 
 
 gost_v1<-readRDS("nf1g/atrx_comps/ds/gost_v1.rds")
@@ -18,7 +19,7 @@ pathway_list_sum<-tibble(pathway_list=NA, index=NA, index_index=NA, p_value=NA)
 for (ps in 1:length(gost_v1)){
   
   pathway_list_sum<-bind_rows(pathway_list_sum,
-                              as.tibble(gost_v1[[ps]]$result)%>%
+                              as_tibble(gost_v1[[ps]]$result)%>%
                                 mutate(index=1:n())%>%
                                 mutate(index_index=ps)%>%
                                 rename(pathway_list=source)%>%
@@ -31,49 +32,48 @@ for (ps in 1:length(gost_v1)){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 pathway_list_tib<-tibble(pathway_list=NA)
 for (de1.5 in 1:length(gost_v1)){
-
-pathway_list_tib<-full_join(pathway_list_tib,
-                            as.tibble(gost_v1[[de1.5]]$result)%>%
-                              rename(pathway_list=source)%>%
-    dplyr::select(pathway_list)%>%unique())%>%
-  unique()%>%
-  drop_na()%>%
-  arrange(pathway_list)
-
-pathway_list_factor<-pathway_list_tib%>%
-  mutate(pathway_list=factor(pathway_list, levels=pathway_list_tib$pathway_list))%>%
-  mutate(group_x_coord=1:n())
-
-
+  
+  pathway_list_tib<-full_join(pathway_list_tib,
+                              as_tibble(gost_v1[[de1.5]]$result)%>%
+                                rename(pathway_list=source)%>%
+                                dplyr::select(pathway_list)%>%unique())%>%
+    unique()%>%
+    drop_na()%>%
+    arrange(pathway_list)
+  
+  pathway_list_factor<-pathway_list_tib%>%
+    mutate(pathway_list=factor(pathway_list, levels=pathway_list_tib$pathway_list))%>%
+    mutate(group_x_coord=1:n())
+  
+  
 }
+
+# 
+# 
+# pathway_palette <- setNames(
+#   scales::hue_pal()(length(pathway_list_factor$pathway_list)),# Ensure this matches pathway_list categories
+#   pathway_list_factor$pathway_list
+# )
+# 
+# 
+# 
+# # Function to lighten color by blending with gray
+# lighten_color <- function(col, factor = 0.5) {
+#   col_rgb <- col2rgb(col) / 255  # Normalize RGB values
+#   gray_rgb <- col_rgb * (1 - factor) + factor  # Blend with gray
+#   rgb(gray_rgb[1], gray_rgb[2], gray_rgb[3])  # Convert back to hex
+# }
+# 
+# # Create a modified version of the palette where high p-values are lightened
+# adjusted_palette <- pathway_palette  # Copy original colors
+# 
+# 
+
+
+
+
 
 
 
@@ -83,7 +83,7 @@ pathway_list_factor<-pathway_list_tib%>%
 
 for (de2 in 1:length(gost_v1)){
 
-
+# de2<-3
   
 cat(blue("working on "), red(names(gost_v1$de2)), " (", cyan(de2), " of ", length(gost_v1), ")", "\n")
   
@@ -117,9 +117,40 @@ data3 <- left_join(pathway_list_factor,data2)%>%
   
   mutate(label_x_pos=if_else(dist_from_group_x<0, group_x_coord-.3, group_x_coord+.3))%>%
   
-  mutate(g_label = if_else(-log10(p_value) > 5|grepl("MHC", term_name), 
+  group_by(pathway_list) %>% #create ranked list for filtering labels.
+  mutate(
+    group_rank = rank(p_value, ties.method = "average"),  # Rank p-values within each pathway
+    group_percentile = (1 - percent_rank(p_value)) * 100  # Convert to percentile (0-100 scale)
+  ) %>%
+  ungroup()%>%
+  mutate(
+    rank = rank(p_value, ties.method = "average"),  # Rank p-values within each pathway
+    percentile = (1 - percent_rank(p_value)) * 100  # Convert to percentile (0-100 scale)
+  )%>%
+  
+  
+######set labelling threshold############################################  
+  mutate(g_label = if_else(-log10(p_value)>5 &
+                            ((grepl("MHC", term_name)&group_percentile>=80) |group_rank<=5|group_percentile>=97),
+                              
                            stringr::str_wrap(term_name, width=25),
+                           
                            NA))
+
+########################################################
+
+
+
+# 
+# high_pvals <- data3$p_value > 5   # Define high p-values
+# 
+# # Apply lightening only to pathways with high p-values
+# adjusted_palette[data3$pathway_list[high_pvals]] <- 
+#   sapply(pathway_palette[data3$pathway_list[high_pvals]], lighten_color, factor = 0.5)
+# 
+# 
+# 
+
 
 
 my_pal <- function(range = c(1, 10)) {
@@ -143,26 +174,33 @@ gost_v1[[de2]]$plot1 <- ggplot(data3, aes(x = group_x_coord*x_scale_factor_man+d
   
   # Add jittered points
   geom_point(aes(size = intersection_size, 
-                 color = pathway_list),
+                 color = pathway_list # Lighten color
+                 ),
              shape = 16,
              alpha = 0.4)+# Ensures both fill & stroke work) +      
   geom_point(aes(size = intersection_size, 
-                 color = pathway_list),
+                 color = pathway_list # Lighten color
+                 ),
              shape = 1,
              stroke=.8,
              alpha = 0.8)+# Ensures both fill & stroke work) +  
   
+  
+  # scale_color_manual(values = setNames(
+  #   desaturate(unique(data3$pathway_list), 0.5),  # Precompute desaturated colors
+  #   unique(data3$pathway_list)
+  # )) +
 
   geom_label_repel(data=subset(data3),
                    aes(label = g_label,
                        point.size = intersection_size),
                    point.padding = .1,
                    #family="Open Sans",
-                   alpha=.8,
-                   size=3,
+                   alpha=.7,
+                   size=2,
                    min.segment.length = 0,
                    segment.alpha=.25,
-                   max.overlaps = 30,
+                   max.overlaps = 500000000,
                    force = 5,
                    hjust= 0.5,
                    vjust=0.5,
@@ -188,7 +226,7 @@ gost_v1[[de2]]$plot1 <- ggplot(data3, aes(x = group_x_coord*x_scale_factor_man+d
   
   continuous_scale(
     aesthetics = c("size", "point.size"),  # Scale both point size and label size
-    palette = my_pal(c(4, 12))  # Custom palette for scaling
+    palette = my_pal(c(1, 8))  # Custom palette for scaling
   ) +
   coord_cartesian(clip = "off")+
   theme_classic()
@@ -220,6 +258,32 @@ gost_v1[[de2]]$plot2 <- arrangeGrob(
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # grid.newpage()
 # grid.draw(gost_v1[[7]]$plot1)
 # 
@@ -231,9 +295,6 @@ gost_v1[[de2]]$plot2 <- arrangeGrob(
 
 plot_output_base<-"gost-"
 plot_output_loc<-"nf1g/atrx_comps/plots/"  #include trailing slash
-
-
-# gost_v1<-gost_v1[7]
 
 
 # Loop through the outer list
