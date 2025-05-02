4af62abc0137fbc78593c794124776daa6f474b2
Garrett Otrimski <u1413890@hci.utah.edu>
2025-05-02 15:43:23 -0600

    tumor penetrance stacked grade 2

--- nf1g/surv/pub/tt/2025-04-28-tumor-splits.R
+++ nf1g/surv/pub/tt/2025-04-28-tumor-splits.R
@@ -10,6 +10,11 @@ library(survival)
 library(survminer)
 library(RColorBrewer)
 
+
+library(ggh4x)
+
+
+
 source("ggplot_draw_square.R")
 
 
@@ -27,7 +32,7 @@ df1<-coh1%>%
 
 prop_var_name<-"hist_grade_name"
 
-
+###initial dataset prep ##################
 df_props<-df1%>% #prep dataset and count proportions of each prop_var_name per genotype.
   
   select(resultant_geno, !!sym(prop_var_name))%>%
@@ -60,46 +65,6 @@ df_props<-df1%>% #prep dataset and count proportions of each prop_var_name per g
   #complete(resultant_geno,!!sym(prop_var_name), fill = list(perc = -.5))%>%
   
  
-
-
-# 
-# 
-# df_props_w <- df_props %>%
-#   select(-perc, -resultant_geno_name)%>%
-#   pivot_wider(
-#     names_from = resultant_geno,    # Pivot by `resultant_geno`
-#     values_from = c(n, total_n),    # Get `n` and `total_n` columns
-#     names_glue = "{resultant_geno}_{.value}"  # Create custom column names like 'genotype_n' and 'genotype_total_n'
-#   )
-# 
-
-
-# results <- df_props_w %>%
-#   rowwise() %>%  # Apply row-wise operations
-#   mutate(
-#     prop_test = list({
-#       # Check if any count is less than a threshold (e.g., 5)
-#       if(any(c(`nf1 KO; pten KO; ink KO; atrx KO_n`, `nf1 KO; pten KO; ink KO; atrx wt_n`) < 5)) {
-#         # Use Fisher's Exact Test for small counts
-#         fisher.test(matrix(c(`nf1 KO; pten KO; ink KO; atrx KO_n`, `nf1 KO; pten KO; ink KO; atrx wt_n`, 
-#                              `nf1 KO; pten KO; ink KO; atrx KO_total_n`, `nf1 KO; pten KO; ink KO; atrx wt_total_n`), 
-#                            nrow = 2))
-#       } else {
-#         # Use prop.test() otherwise
-#         prop.test(
-#           x = c(`nf1 KO; pten KO; ink KO; atrx KO_n`, `nf1 KO; pten KO; ink KO; atrx wt_n`),  # Success counts (x)
-#           n = c(`nf1 KO; pten KO; ink KO; atrx KO_total_n`, `nf1 KO; pten KO; ink KO; atrx wt_total_n`)  # Total counts (n)
-#         )
-#       }
-#     }),
-#     p_value = prop_test$p.value,
-#     estimate_WT = prop_test$estimate[1],  # Estimate for WT genotype
-#     estimate_KO = prop_test$estimate[2]   # Estimate for KO genotype
-#   ) %>%
-#   select(!!sym(prop_var_name), p_value, estimate_WT, estimate_KO)  # Adjust columns as needed
-
-
-
 
 tumor_counts <- table(df1$resultant_geno,  df1[[prop_var_name]])  # Contingency table
 total_counts <- rowSums(tumor_counts)  # Total tumors per genotype
@@ -136,7 +101,7 @@ stats_list<-list(meta=list(src=rstudioapi::getSourceEditorContext()$path%>%
 
 
 
-
+## 3group (Tumor/No tumor/pretumor) ####################
 ####### 3g prep#############
 
 df_props_3g <- df_props %>%
@@ -178,7 +143,10 @@ pairwise_tests3g <- combn(1:nrow(prop_tests_df), 2, simplify = FALSE) %>%
     )
     
     fisher_p <- fisher.test(mat, simulate.p.value = TRUE, B = 1e5)$p.value
-    chi_p <- chisq.test(mat, correct = FALSE)$p.value
+    
+    suppressWarnings(
+      chi_p <- chisq.test(mat, correct = FALSE)$p.value
+      )
     
     tibble(
       group1 = as.character(g1$resultant_geno),
@@ -188,112 +156,15 @@ pairwise_tests3g <- combn(1:nrow(prop_tests_df), 2, simplify = FALSE) %>%
     )
   }) %>%
   arrange(group1, group2)
-
-
-
-
-
-####### 2g prep#############
-
-df_props_2g <- df_props %>%
-  mutate(tumor = if_else(hist_grade_name %in% c("No evidence of disease", "Pretumorigenic", "Excluded from histology (no event)"),
-                         "No tumor", "Tumor"))%>%
-
-  mutate(perc=if_else(perc<0, NA, perc))%>%
-  filter(!is.na(perc))%>%
-  
-  mutate(resultant_geno_label=str_wrap(resultant_geno, width=8))%>%
-  
-  mutate(tumor=factor(tumor, levels=c("Tumor", "No tumor")))
-
-
-
-
-############## 3g stats #############
-
-prop_tests_df<-df_props_2g%>%
-  select(resultant_geno, hist_grade_name, n, tumor)%>%
-  group_by(resultant_geno, tumor)%>%
-  summarise(ncount=sum(n))%>%
-  pivot_wider(values_from = ncount,  names_from = tumor)%>%
-  janitor::clean_names()%>%
-  mutate(total_n=tumor+no_tumor)
-
-
-
-# All pairwise 2x3 comparisons
-pairwise_tests2g <- combn(1:nrow(prop_tests_df), 2, simplify = FALSE) %>%
-  map_df(~{
-    g1 <- prop_tests_df[.x[1], ]
-    g2 <- prop_tests_df[.x[2], ]
-    
-    mat <- rbind(
-      c(g1$tumor,g1$no_tumor),
-      c(g2$tumor, g2$no_tumor)
-    )
-    
-    fisher_p <- fisher.test(mat, simulate.p.value = TRUE, B = 1e5)$p.value
-    chi_p <- chisq.test(mat, correct = FALSE)$p.value
-    
-    tibble(
-      group1 = as.character(g1$resultant_geno),
-      group2 = as.character(g2$resultant_geno),
-      fisher_p = fisher_p,
-      chisq_p = chi_p
-    )
-  }) %>%
-  arrange(group1, group2)
-
-
-
-
-
-#######################################
-
-
-
-
-
-
-
-
-
-
-
-
-table_data <- with(df_props1, tapply(n, list(hist_grade_name, resultant_geno), sum))
-chisq.test(table_data)
-
-
-
-library(nnet)
-df_props1$hist_grade_name <- relevel(df_props1$hist_grade_name, ref = "No evidence of disease")
-model <- multinom(hist_grade_name ~ resultant_geno, weights = n, data = df_props1)
-summary(model)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ####### plot5 start########################################################################### 
 
 
-p5<-ggplot(df_props_pen0, aes(x = tumor, y = perc, fill=hist_grade_name)) +
+p3g<-ggplot(df_props_3g, aes(x = tumor, y = perc, fill=hist_grade_name)) +
   geom_bar(stat = "identity", position = "stack", width = 0.9, color="black")+
   facet_grid(~resultant_geno_label)+
   
- scale_fill_manual(values=col_map$hist_grade_name)+
+  scale_fill_manual(values=col_map$hist_grade_name)+
   
   
   theme_pubclean()+
@@ -305,7 +176,62 @@ p5<-ggplot(df_props_pen0, aes(x = tumor, y = perc, fill=hist_grade_name)) +
 
 
 
+labels<-df_props_3g%>%
+  left_join(tibble(
+    resultant_geno = names(col_map$resultant_geno),
+    color_hex = col_map$resultant_geno
+  )
+  )%>%
+  select(resultant_geno_label, color_hex)%>%
+  unique()
 
+
+# 
+# facet_levels <- levels(df_props_3g$resultant_geno)
+# facet_colors <- col_map$resultant_geno[facet_levels]
+
+
+strip_elements <- Map(function(fill) {
+  element_rect(fill = fill, colour = "black", linewidth = 0.5)
+}, labels$color_hex)
+
+
+# Build plot
+ggplot(df_props_3g, aes(x = tumor, y = perc, fill = hist_grade_name)) +
+  geom_bar(stat = "identity", position = "stack", width = 0.9, color = "black") +
+  
+  # Apply in strip_themed
+  facet_grid2(~resultant_geno_label,
+              strip = strip_themed(
+                background_x = strip_elements,
+                text_x = element_text(size = 8)
+              ))+
+  
+  scale_fill_manual(values = col_list$hist_grade_name) +
+  
+  theme_pubclean() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+    legend.position = "right",
+    legend.key.size = unit(0.4, "cm"),
+    strip.text.x = element_text(size = 11, face = "bold", color = "black")
+  ) +
+  labs(x = NULL, y = "% of cohort")
+
+
+
+
+
+
+col_map$resultant_geno
+
+
+df_props_3g$resultant_geno%>%unique()
+
+
+
+
+setdiff(levels(df_props_3g$resultant_geno), names(col_map$resultant_geno))
 
 ####### save plot5 start####
 metadata_text <- paste0("src: ", 
@@ -317,7 +243,7 @@ metadata_text <- paste0("src: ",
 
 text_grob <- textGrob(metadata_text, x=.05, just="left", gp = gpar(fontsize = 9, col = "gray30"))
 
-p5src<-grid.arrange(p5, text_grob, ncol = 1, heights = c(3, 0.3))
+p3g_gr<-grid.arrange(p3g, text_grob, ncol = 1, heights = c(3, 0.3))
 
 
 
@@ -335,7 +261,7 @@ ggsave("nf1g/surv/pub/pub_plots/penetrance-stacked-grade0.pdf",
                     lubridate::round_date(Sys.time(), "second")
        ),
        
-       plot=p5src,
+       plot=p3g_gr,
        limitsize = FALSE,
        
        
@@ -360,7 +286,7 @@ ggsave("nf1g/surv/pub/pub_plots/penetrance-stacked-grade0b.pdf",
                     lubridate::round_date(Sys.time(), "second")
        ),
        
-       plot=p5src,
+       plot=p3g_gr,
        limitsize = FALSE,
        
        
@@ -372,5 +298,252 @@ ggsave("nf1g/surv/pub/pub_plots/penetrance-stacked-grade0b.pdf",
        
        
 )  
+
+
+###### end of plot saves #########################
+## 2group (Tumor/No) ###################
+####### 2g prep#############
+
+df_props_2g <- df_props %>%
+  mutate(tumor = if_else(hist_grade_name %in% c("No evidence of disease", "Pretumorigenic", "Excluded from histology (no event)"),
+                         "No tumor", "Tumor"))%>%
+
+  mutate(perc=if_else(perc<0, NA, perc))%>%
+  filter(!is.na(perc))%>%
+  
+  mutate(resultant_geno_label=str_wrap(resultant_geno, width=8))%>%
+  
+  mutate(tumor=factor(tumor, levels=c("Tumor", "No tumor")))
+
+
+############## 2g stats #############
+
+prop_tests_df<-df_props_2g%>%
+  select(resultant_geno, hist_grade_name, n, tumor)%>%
+  group_by(resultant_geno, tumor)%>%
+  summarise(ncount=sum(n))%>%
+  pivot_wider(values_from = ncount,  names_from = tumor)%>%
+  janitor::clean_names()%>%
+  mutate(total_n=tumor+no_tumor)
+
+
+
+# All pairwise 2x2 comparisons
+pairwise_tests2g <- combn(1:nrow(prop_tests_df), 2, simplify = FALSE) %>%
+  map_df(~{
+    g1 <- prop_tests_df[.x[1], ]
+    g2 <- prop_tests_df[.x[2], ]
+    
+    mat <- rbind(
+      c(g1$tumor,g1$no_tumor),
+      c(g2$tumor, g2$no_tumor)
+    )
+    
+    fisher_p <- fisher.test(mat, simulate.p.value = TRUE, B = 1e5)$p.value
+    chi_p <- suppressWarnings(
+      chisq.test(mat, correct = FALSE)$p.value
+    )
+    tibble(
+      group1 = as.character(g1$resultant_geno),
+      group2 = as.character(g2$resultant_geno),
+      fisher_p = fisher_p,
+      chisq_p = chi_p
+    )
+  }) %>%
+  arrange(group1, group2)
+
+
+
+####### 2g plot start########################################################################### 
+
+
+p2g<-ggplot(df_props_2g, aes(x = tumor, y = perc, fill=hist_grade_name)) +
+  geom_bar(stat = "identity", position = "stack", width = 0.9, color="black")+
+  facet_grid(~resultant_geno_label)+
+  
+  scale_fill_manual(values=col_map$hist_grade_name)+
+  
+  
+  theme_pubclean()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+        guides(fill = guide_legend(nrow = 9)))+  # Adjust size as needed
+  labs(x = NULL,
+       y="% of cohort")
+
+
+
+
+
+
+####### save plot5 start####
+metadata_text <- paste0("src: ", 
+                        rstudioapi::getSourceEditorContext()$path %>%
+                          sub("/uufs/chpc.utah.edu/common/home/holmen-group1/otrimskig/","",.) %>%
+                          sub("C:/Users/u1413890/OneDrive - University of Utah/garrett hl-onedrive/R/","",.), 
+                        "\n", "time: ", 
+                        lubridate::round_date(Sys.time(), "second"))
+
+text_grob <- textGrob(metadata_text, x=.05, just="left", gp = gpar(fontsize = 9, col = "gray30"))
+
+p2g_gr<-grid.arrange(p2g, text_grob, ncol = 1, heights = c(3, 0.3))
+
+
+
+
+ggsave("nf1g/surv/pub/pub_plots/penetrance-stacked-grade0-2g.pdf",
+       
+       title=paste0("src: ",
+                    
+                    rstudioapi::getSourceEditorContext()$path%>%
+                      sub("/uufs/chpc.utah.edu/common/home/holmen-group1/otrimskig/","",.)%>%
+                      sub("C:/Users/u1413890/OneDrive - University of Utah/garrett hl-onedrive/R/","",.),
+                    
+                    " at ", 
+                    
+                    lubridate::round_date(Sys.time(), "second")
+       ),
+       
+       plot=p2g_gr,
+       limitsize = FALSE,
+       
+       
+       height=3,
+       width=5,
+       scale = 4,
+       dpi=600,
+       
+       
+       
+)  
+ggsave("nf1g/surv/pub/pub_plots/penetrance-stacked-grade0-2g-b.pdf",
+       
+       title=paste0("src: ",
+                    
+                    rstudioapi::getSourceEditorContext()$path%>%
+                      sub("/uufs/chpc.utah.edu/common/home/holmen-group1/otrimskig/","",.)%>%
+                      sub("C:/Users/u1413890/OneDrive - University of Utah/garrett hl-onedrive/R/","",.),
+                    
+                    " at ", 
+                    
+                    lubridate::round_date(Sys.time(), "second")
+       ),
+       
+       plot=p2g_gr,
+       limitsize = FALSE,
+       
+       
+       height=5,
+       width=3.5,
+       scale = 2,
+       dpi=600,
+       
+       
+       
+)  
+
+
+###### end of plot saves #########################
+
+
+
+df_props_stack0<-df_props_2g%>%
+  mutate(hist_grade_name=factor(hist_grade_name, 
+                                
+                                levels=rev(c("Excluded from histology (event)", 
+                                                          "No grade assigned", 
+                                         "Grade 4", "Grade 3", "Grade 2", "Grade 1",
+                                         "Pretumorigenic", "No evidence of disease",
+                                         "Excluded from histology (no event)"))))
+
+df_stack_pen<-df_props_stack0%>%
+  group_by(resultant_geno)%>%
+  filter(tumor=="Tumor")%>%
+  summarise(pen=sum(perc))%>%
+  left_join(col_df)
+
+
+
+
+df_props_stack1<-df_props_stack0%>%
+  left_join(df_stack_pen)%>%
+  left_join(col_df)
+
+
+
+pstack<-ggplot(df_props_stack1, aes(x =resultant_geno, y = perc, fill=hist_grade_name)) +
+  geom_bar(stat = "identity", position = "stack", width = 0.7, #color="black", 
+           alpha=.7)+
+  #facet_grid(~resultant_geno_label)+
+  
+  scale_fill_manual(values=col_map$hist_grade_name)+
+  
+  # 
+  # geom_tile(data=df_stack_pen, aes(x=resultant_geno,
+  #                                  y=-1    #pen
+  #                                  ),
+  #           fill=df_stack_pen$hex_col,
+  #           height=.8,
+  #           width=.8,
+  #           #fill="black",
+  #           #color="black",
+  #           alpha=1)+
+
+  
+  
+  geom_tile(data=df_stack_pen, aes(x=resultant_geno, 
+                                   y=pen
+  ), 
+  fill="black", 
+  height=.2,
+  width=.8,
+  #fill="black", 
+  #color="black", 
+  alpha=1)+
+  
+  
+  
+  
+  
+  
+ 
+  geom_label(
+    aes(y = pen, label = paste0(round(pen), "%")),
+    fill = "white",
+    color = NA,                 # no border
+    label.r = unit(0, "pt"),    # square corners
+    size = 3
+  ) +
+  
+  # Black text overlaid
+  geom_text(
+    aes(y = pen, label = paste0(round(pen), "%")),
+    color = "black",
+    size = 3
+  )+
+ theme_pubr()+
+  theme(axis.text.x = element_text(angle = 45, 
+                                   hjust = 1, 
+                                   size = 12),
+        guides(fill = guide_legend(nrow = 9)))+  # Adjust size as needed
+  labs(x = NULL,
+       y="% of cohort")+
+  scale_y_continuous(expand = expansion(mult = c(0.0, 0.0)))+
+  coord_flip()
+ 
+
+
+pstack
+# 
+# table_data <- with(df_props1, tapply(n, list(hist_grade_name, resultant_geno), sum))
+# chisq.test(table_data)
+# 
+# 
+# 
+# library(nnet)
+# df_props1$hist_grade_name <- relevel(df_props1$hist_grade_name, ref = "No evidence of disease")
+# model <- multinom(hist_grade_name ~ resultant_geno, weights = n, data = df_props1)
+# summary(model)
+# 
+
 
 
